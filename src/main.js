2e02dff2119af2a3d7decde794a4263e08c9c0a1
Kirill Stytsenko <kirstyts@gmail.com>
2019-04-25 20:20:00 +0200

    Add SvgIcon styles

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import './plugins/vuetify'
 import store from './store'
 import Vuetify from 'vuetify'
 import VueI18n from 'vue-i18n'
-import * as svgicon from 'vue-svgicon'
+import SvgIcon from 'vue-svgicon'
 import eng from './i18n/eng'
 import deu from './i18n/deu'
 import ita from './i18n/ita'
@@ -14,7 +14,7 @@ Vue.config.productionTip = false
 
 Vue.use(Vuetify)
 Vue.use(VueI18n)
-Vue.use(svgicon, {
+Vue.use(SvgIcon, {
   tagName: 'icon',
   defaultWidth: '1em',
   defaultHeight: '1em'
