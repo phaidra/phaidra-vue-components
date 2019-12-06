6d849d61500615cb0e50318c0e3b576e40d717a8
Giulio <giulio.turetta@unipd.it>
2019-12-07 00:26:26 +0100

    whitespace

--- src/components/display/PDDimension.vue
+++ src/components/display/PDDimension.vue
@@ -1,7 +1,7 @@
 <template>
   <v-row>
     <v-col md="2" cols="12" class="pdlabel primary--text text-right">{{ $t(p) }}</v-col>
-    <v-col md="10" cols="12"><span v-for="(v, i) in o['schema:value']" :key="'v'+i">{{ v }}</span> <span v-for="(v, i) in o['schema:unitCode']" :key="'u'+i">{{ getLocalizedTermLabel('uncefact', v) }}</span></v-col>
+    <v-col md="10" cols="12"><span v-for="(v, i) in o['schema:value']" :key="'v'+i">{{ v }}</span>&nbsp;<span v-for="(v, i) in o['schema:unitCode']" :key="'u'+i">{{ getLocalizedTermLabel('uncefact', v) }}</span></v-col>
   </v-row>
 </template>
 
