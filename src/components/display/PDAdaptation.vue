1f2eb4f0da138738adf2456f0252fcd0bf929d10
Rasta <hudak.rastislav@gmail.com>
2022-03-08 13:01:44 +0100

    card

--- src/components/display/PDAdaptation.vue
+++ src/components/display/PDAdaptation.vue
@@ -2,7 +2,7 @@
   <v-row>
     <v-col :md="labelColMd" cols="12" class="pdlabel primary--text text-md-right">{{ $t(p) }}</v-col>
     <v-col :md="valueColMd" cols="12">
-      <v-card flat>
+      <v-card tile elevation="0">
         <v-card-text class="border-left">
           <v-container fluid>
             <v-row>
