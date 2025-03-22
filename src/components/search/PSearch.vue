4aad1173665c26ee9da8ca5722860b1c2fc98ff3
Rastislav Hudak <rastislav.hudak@univie.ac.at>
2025-03-22 17:20:47 +0100

    csv export fields

--- src/components/search/PSearch.vue
+++ src/components/search/PSearch.vue
@@ -173,7 +173,7 @@ export default {
         params.rows = response.data.response.numFound
         params.indent = 'on'
         params.wt = 'csv'
-        params.fl = ['pid', 'dc_title', 'dc_creator', 'bib_published']
+        params.fl = ['pid', 'dc_title', 'dc_creator', 'dc_contributor', 'dc_description', 'language', 'keywords:keyword_suggest', 'dc_rights', 'bib_published', 'dc_identifier', 'owner', 'dc_format','edm_hastype','resourcetype','created', 'modified', 'size','ismemberof']
         params['fl.alias'] = ''
         const csvquery = qs.stringify(params, { encodeValuesOnly: true, indices: false })
         this.$axios.request('/search/select?' + csvquery, {
